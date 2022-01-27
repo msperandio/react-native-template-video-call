7f6167fa7692d195f160f9dd4bb0a54eb2915c99
Sperandio Matteo <m.sperandio@almaviva.it>
2022-01-27 01:11:26 +0100

    First run test

---
+++ MyApp/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, LogBox, StatusBar } from 'react-native';
+import AppWithNavigationState from 'navigators';
+import {} from 'components';
+import GlobalLoading, { globalLoadingRef } from 'components/GlobalLoading';
+import GlobalMessage, { globalMessageRef } from 'components/GlobalMessage';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'shared/language';
+import {
+  globalCallRef,
+  GlobalCallUI,
+  globalGroupCallRef,
+  GlobalGroupCallUI,
+} from 'react-native-webrtc-simple/UIKit';
+
+LogBox.ignoreAllLogs(true);
+StatusBar.setBarStyle('dark-content');
+const MyApp = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <I18nextProvider i18n={i18next}>
+        <AppWithNavigationState />
+        <GlobalLoading ref={globalLoadingRef} />
+        <GlobalMessage ref={globalMessageRef} />
+        <GlobalCallUI ref={globalCallRef} />
+        <GlobalGroupCallUI ref={globalGroupCallRef} />
+      </I18nextProvider>
+    </View>
+  );
+};
+export default MyApp;
---
+++ MyApp/src/screens/group/index.tsx
@@ -0,0 +1,62 @@
+import { Button, Header } from 'components';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View } from 'react-native';
+import { TextInput } from 'react-native-element-textinput';
+import { useAppState } from 'react-native-utils-toolkit';
+import WebrtcSimple from 'react-native-webrtc-simple';
+import { globalGroupCall } from 'react-native-webrtc-simple/UIKit';
+import { styles } from './styles';
+
+interface Props {
+  fullName: string;
+  sessionId: string;
+}
+
+const GroupScreen: React.FC<Props> = props => {
+  const { fullName, sessionId: myId } = props;
+  const [sessionId, setSessionId] = useState<string[]>([]);
+
+  const startGroup = (sessionId: string[]) => {
+    const data = {
+      name: 'Group name',
+      avatar:
+        'https://www.atlantawatershed.org/wp-content/uploads/2017/06/default-placeholder.png',
+    };
+    globalGroupCall.call(sessionId, data);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header
+        name={`${fullName}: ${myId}`}
+        onPressUser={() => {}}
+        onChangeText={e => {}}
+      />
+
+      <View style={styles.boxMyStream}>
+        <View style={styles.wrap}>
+          <TextInput
+            style={styles.input}
+            placeholder="Session Id..."
+            placeholderTextColor="gray"
+            hashtagValue={sessionId}
+            onChangeHashtag={value => {
+              setSessionId(value);
+            }}
+            focusColor="red"
+            autoCapitalize={'none'}
+          />
+          <View style={styles.wrapButton}>
+            <Button
+              style={styles.button}
+              title="Start"
+              onPress={() => startGroup(sessionId)}
+            />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default GroupScreen;
