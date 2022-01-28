5d20b719b90ec70a330e25b8351a23994a956851
Sperandio Matteo <m.sperandio@almaviva.it>
2022-01-28 01:08:28 +0100

    Started prj StaSuonando

--- MyApp/src/components/FlatList/type.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { FlatListProps } from 'react-native';
-
-interface Props extends FlatListProps<any> {
-  onRefresh?: () => void;
-  onLoadMore?: () => void;
-}
-
-export type PropsFlatlist = React.FC<Props>;
--- MyApp/src/components/Text/type.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { TextProps } from 'react-native';
-interface Props extends TextProps {
-  fontSize?: number | undefined;
-  bold?: boolean;
-  color?: string | undefined;
-}
-
-export type PropsText = React.FC<Props>;
